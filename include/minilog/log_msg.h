88da350f0289b2534e3485b27c4b523099a4a1b0
hanjing <hanjing2020@foxmail.com>
2023-12-13 23:11:00 +0800

    add time and  source location

--- include/minilog/log_msg.h
+++ include/minilog/log_msg.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <chrono>
+#include <source_location>
 
 #include "minilog/common.h"
 
@@ -8,11 +10,12 @@ namespace minilog {
 struct log_msg {
     log_msg() = default;
     
-    log_msg(const std::string &name, level::level_enum lvl, std::string_view msg) : logger_name(name), level(lvl), payload(msg) {}
+    log_msg(const std::string &name, level::level_enum lvl, std::string_view msg, std::source_location loc=std::source_location::current()) : logger_name(name), level(lvl), payload(msg), location(loc) {}
 
     std::string_view logger_name;
     level::level_enum level{level::off};
     std::string_view payload;
-
+    std::chrono::zoned_time<std::chrono::system_clock::duration> time{std::chrono::current_zone(), std::chrono::system_clock::now()};
+    std::source_location location;
 };
 }
